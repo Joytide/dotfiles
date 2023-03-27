fffb53ebcc31883723dc5fac4c8f71cc7eb22958
Joytide <44203266+Joytide@users.noreply.github.com>
2023-03-27 17:46:25 +0200

    Update right_status.sh

--- tmux/.tmux/right_status.sh
+++ tmux/.tmux/right_status.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
-echo -n "["
-[ -d /proc/sys/net/ipv4/conf/wlp4s0 ] && echo -n "wlp4s0: "
-[ -d /proc/sys/net/ipv4/conf/wlp4s0 ] && echo -n "$({ ip -4 -br a sh dev wlp4s0 | awk {'print $3'} | cut -f1 -d/; } 2>/dev/null)"
-echo -n "] - ["
-[ -d /proc/sys/net/ipv4/conf/tun0 ] && echo -n "tun0: "
-[ -d /proc/sys/net/ipv4/conf/tun0 ] && echo -n "$({ ip -4 -br a sh dev tun0 | awk {'print $3'} | cut -f1 -d/; } 2>/dev/null)"
-echo -n "]"
+#!/bin/bash
+
+output=""
+for iface in $(ip -o link show | awk -F': ' '{print $2}') ; do
+    if ip addr show dev $iface | grep -q 'inet ' && ip link show $iface | grep -q 'state UP'; then
+        ip=$(ip addr show dev $iface | awk '/inet /{print $2}' | cut -d'/' -f1)
+        iface_short=$(echo $iface | cut -c 1-4)
+        output+="[$iface_short: $ip] - "
+    fi
+done
+
+if [ -n "$output" ]; then
+    echo "${output::-3}"
+else
+    echo "[Pas de connexion aux Internets Mondiaux]"
+fi
