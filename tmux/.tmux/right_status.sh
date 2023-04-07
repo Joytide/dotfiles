69210193afa13fd09c16ab14da00d58d28e19ad0
Joytide <44203266+Joytide@users.noreply.github.com>
2023-04-07 17:45:54 +0200

    Update right_status.sh

--- tmux/.tmux/right_status.sh
+++ tmux/.tmux/right_status.sh
@@ -2,15 +2,18 @@
 
 output=""
 for iface in $(ip -o link show | awk -F': ' '{print $2}') ; do
-    if ip addr show dev $iface | grep -q 'inet ' && ip link show $iface | grep -q 'state UP'; then
-        ip=$(ip addr show dev $iface | awk '/inet /{print $2}' | cut -d'/' -f1)
-        iface_short=$(echo $iface | cut -c 1-4)
-        output+="[$iface_short: $ip] - "
+    if [[ $iface == lo* ]]; then
+        continue  # skip loopback interface
+    fi
+    ip_addr=$(ip -o -4 addr show dev $iface | awk '{print $4}')
+    if [ -n "$ip_addr" ]; then
+        iface_short=$(echo $iface | cut -c 1-6)
+        output+="[$iface_short: ${ip_addr%/*}] - "
     fi
 done
 
 if [ -n "$output" ]; then
-    echo "${output::-3}"
+    echo "${output::-3} |"
 else
-    echo "[Pas de connexion aux Internets Mondiaux]"
+    echo "[Pas de connexion aux Internet Mondiaux]"
 fi
